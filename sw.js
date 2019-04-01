dfa90ceefd221d1f756519e237356575a620e13b
xiemeng <945669651@qq.com>
2019-04-01 21:08:00 +0800

    提交代码

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-var cacheName = 'hello world';
+var cacheName = 'hellWorld';
 self.addEventListener('install',event => {
 	event.waitUntil(
 		caches.open(cacheName).then(cache =>{
+			console.log(cache)
 			cache.addAll([
-				'/yong.png'
+				'./yong.png',
+				'./self.html'
 			])
+		})
+	)
+})
+self.addEventListener('fetch',(event)=>{
+	event.respondWith(
+		caches.match(event.request).then((response)=>{
+			console.log(response)
+			if(response){
+				return response
+			}
+			return fetch(event.request)
 		})
 	)
 })
